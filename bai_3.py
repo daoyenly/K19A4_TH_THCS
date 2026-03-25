95a39aeefc291e26d4f0715afec157227d05aaf8
daoyenly <dyly.24174600101@sv.uneti.edu.vn>
2026-03-25 12:24:48 +0700

    Delete bai_3.py

--- bai_3.py
+++
@@ -1,9 +0,0 @@
-while True:
-    s = input("Nhap so: ")
-    try:
-        x = float(s)
-        if x < 0 or not x.is_integer():
-            print("Dung chuong trinh!")
-            break
-    except:
-        print("Khong hop le!")
